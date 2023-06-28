f08be3ee672a35f41400e8e6741a116928fcb29a
Allen Mathews <allenjoemathews@gmail.com>
2023-06-29 08:27:19 +1000

    added changes to script files

--- js/dist/script.dev.js
+++ js/dist/script.dev.js
@@ -1,7 +1,146 @@
 "use strict";
 
-var _data = require("/data.js");
-
+var catsData = [{
+  emotionTags: ["moody"],
+  isGif: false,
+  image: "angry.jpeg",
+  alt: "A cat looking moody"
+}, {
+  emotionTags: ["moody", "insomniac"],
+  isGif: false,
+  image: "angry2.jpeg",
+  alt: "A cat looking moody"
+}, {
+  emotionTags: ["moody"],
+  isGif: false,
+  image: "angry3.jpeg",
+  alt: "A cat looking moody"
+}, {
+  emotionTags: ["confused", "sad"],
+  isGif: false,
+  image: "confused.jpeg",
+  alt: "A cat looking confused"
+}, {
+  emotionTags: ["dominant", "moody"],
+  isGif: false,
+  image: "dominant.jpeg",
+  alt: "A cat looking dominant"
+}, {
+  emotionTags: ["happy", "relaxed"],
+  isGif: false,
+  image: "happy.jpeg",
+  alt: "A cat looking happy"
+}, {
+  emotionTags: ["hungry"],
+  isGif: false,
+  image: "hungry.jpeg",
+  alt: "A cat looking hungry"
+}, {
+  emotionTags: ["hungry"],
+  isGif: false,
+  image: "hungry1.jpeg",
+  alt: "A cat looking hungry"
+}, {
+  emotionTags: ["insomniac"],
+  isGif: false,
+  image: "insomnia.jpeg",
+  alt: "A cat looking insomniac"
+}, {
+  emotionTags: ["insomniac"],
+  isGif: false,
+  image: "insomnia1.jpeg",
+  alt: "A cat looking insomniac"
+}, {
+  emotionTags: ["relaxed"],
+  isGif: false,
+  image: "lazy.jpeg",
+  alt: "A cat looking lazy"
+}, {
+  emotionTags: ["scared"],
+  isGif: false,
+  image: "nervous.jpeg",
+  alt: "A cat looking nervous"
+}, {
+  emotionTags: ["sad"],
+  isGif: false,
+  image: "sad.jpeg",
+  alt: "A cat looking sad"
+}, {
+  emotionTags: ["sad", "moody"],
+  isGif: false,
+  image: "sad1.jpeg",
+  alt: "A cat looking sad"
+}, {
+  emotionTags: ["moody"],
+  isGif: true,
+  image: "angry.gif",
+  alt: "A cat looking moody"
+}, {
+  emotionTags: ["moody"],
+  isGif: true,
+  image: "angry2.gif",
+  alt: "A cat looking angry"
+}, {
+  emotionTags: ["confused"],
+  isGif: true,
+  image: "confused2.gif",
+  alt: "A cat looking confused"
+}, {
+  emotionTags: ["dominant"],
+  isGif: true,
+  image: "dominant.gif",
+  alt: "A cat looking dominant"
+}, {
+  emotionTags: ["happy"],
+  isGif: true,
+  image: "happy.gif",
+  alt: "A cat looking happy"
+}, {
+  emotionTags: ["hungry", "sad", "confused"],
+  isGif: true,
+  image: "confused.gif",
+  alt: "A cat looking hungry"
+}, {
+  emotionTags: ["hungry"],
+  isGif: true,
+  image: "hungry.gif",
+  alt: "A cat looking hungry"
+}, {
+  emotionTags: ["hungry"],
+  isGif: true,
+  image: "hungry2.gif",
+  alt: "A cat looking hungry"
+}, {
+  emotionTags: ["insomniac", "scared"],
+  isGif: true,
+  image: "insomnia2.gif",
+  alt: "A cat looking insomniac"
+}, {
+  emotionTags: ["relaxed"],
+  isGif: true,
+  image: "lazy.gif",
+  alt: "A cat looking relaxed"
+}, {
+  emotionTags: ["relaxed"],
+  isGif: true,
+  image: "relaxed2.gif",
+  alt: "A cat looking relaxed"
+}, {
+  emotionTags: ["scared", "sad"],
+  isGif: true,
+  image: "nervous.gif",
+  alt: "A cat looking nervous"
+}, {
+  emotionTags: ["scared"],
+  isGif: true,
+  image: "nervous2.gif",
+  alt: "A cat looking scared"
+}, {
+  emotionTags: ["sad"],
+  isGif: true,
+  image: "sad.gif",
+  alt: "A cat looking sad"
+}];
 var emotionRadios = document.getElementById('emotion-radios');
 var getImageBtn = document.getElementById('get-image-btn');
 var gifsOnlyOption = document.getElementById('gifs-only-option');
@@ -66,15 +205,13 @@ function getMatchingCatsArray() {
   if (document.querySelector('input[type="radio"]:checked')) {
     var selectedEmotion = document.querySelector('input[type="radio"]:checked').value;
     var isGif = gifsOnlyOption.checked;
-
-    var matchingCatsArray = _data.catsData.filter(function (cat) {
+    var matchingCatsArray = catsData.filter(function (cat) {
       if (isGif) {
         return cat.emotionTags.includes(selectedEmotion) && cat.isGif;
       } else {
         return cat.emotionTags.includes(selectedEmotion);
       }
     });
-
     return matchingCatsArray;
   }
 }
@@ -163,4 +300,4 @@ function renderEmotionsRadios(cats) {
   emotionRadios.innerHTML = radioItems;
 }
 
-renderEmotionsRadios(_data.catsData);
+renderEmotionsRadios(catsData);
